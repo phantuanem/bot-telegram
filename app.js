10de8d2668827ae905a0196b7bf6278d11a9c062
Phan Tuấn Em <83238761+phantuanem@users.noreply.github.com>
2024-04-01 08:31:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const bot = require('./src/telegram.js')
 require('dotenv').config()
-const path = require('path')
 
 bot.launch()
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000)
